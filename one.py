17573110bd3c5d871827d529d63fdf4b8cea94ce
kprogrammingcourse <kprogrammingcourse@gmail.com>
2026-04-25 16:42:23 -0600

    resta agregada

--- one.py
+++ one.py
@@ -2,9 +2,20 @@ print("Hello World")
 
 def myCalculator(param1,param2):
     suma = param1 + param2
-    
-    print(suma)
-    
+    resta = param1 - param2
 
-myCalculator(3,2)
+    print(suma)
+    print(resta)
+
+myCalculator(4,2)
+myCalculator(4,2)
+
+
+
+# git init 
+# git add .
+# git commit -m
+# git status
+
+
     
